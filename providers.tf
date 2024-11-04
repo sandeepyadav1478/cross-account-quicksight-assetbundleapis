1607f2c272e1bfcab135a8705d261bb982dbc2b4
Sandeep Yadav <sandeep.yadav@auctane.com>
2024-11-04 17:42:59 +0530

    providers variables update

--- providers.tf
+++ providers.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   alias      = "source"
-  region     = "us-east-1"
+  region     = var.source_aws_region
   access_key = var.source_aws_access_key
   secret_key = var.source_aws_secret_key
 }
 
 provider "aws" {
   alias      = "target"
-  region     = "us-east-1"
+  region     = var.target_aws_region
   access_key = var.target_aws_access_key
   secret_key = var.target_aws_secret_key
 }
